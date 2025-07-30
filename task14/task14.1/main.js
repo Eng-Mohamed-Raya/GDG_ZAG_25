06831530356307858078fe5f558dfec5e26d5ab7
Eng-Mohamed-Raya <mohamedyasserraya@gmail.com>
2025-07-30 18:32:44 +0300

    add Task 14

---
+++ task14/task14.1/main.js
@@ -0,0 +1,48 @@
+
+
+// ============================== Assignment 2 ==============================  
+        /*
+            var p=document.createElement("p");
+            p.appendChild(document.createTextNode("Elzero"));
+            document.body.appendChild(p);
+            p.style=`
+            color: blue;
+            font-size: 80px;
+            font-weight: bold;
+            text-align: center;
+            font-family: Arial
+            `
+        */
+
+// ============================== Assignment 3 ==============================  
+
+        //console.log("%cMohamed %cYasser %cRaya","color:red;font-size:50px","color:green;font-size:50px; font-weight:900","background-color:blue;font-size:50px")
+
+// ============================== Assignment 4 ==============================  
+
+        /*
+
+            console.group("Group 1")
+            console.log("Message One")
+            console.log("Message Two")
+
+            console.group("Child Group")
+            console.log("Message One")
+            console.log("Message Two")
+
+            console.group("Child Group")
+            console.log("Message One")
+            console.log("Message Two")
+            console.groupEnd();
+            console.groupEnd();
+            console.groupEnd();
+
+            console.group("Group 2")
+            console.log("Message One")
+            console.log("Message Two")
+            console.groupEnd();
+
+        */
+// ============================== Assignment 5 ==============================  
+
+         // console.table(["one","two","three","four","five"]);
